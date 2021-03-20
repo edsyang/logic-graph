83ae1570a037b02b6f33dce3763a285be50ccae6
edsyang <edsyang1102@gmail.com>
2021-03-21 00:19:15 +0800

    feat: mock save request

--- src/logic/panes/header-pane.tsx
+++ src/logic/panes/header-pane.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react';
 import { Button, message } from 'antd';
-// import { MacCommandOutlined } from '@ant-design/icons';
-
-const saveProcess = () => {
-  const random: number = Math.floor(Math.random() * 10);
-  random > 5 ? message.success('success') : message.error('error');
-};
 
 const HeaderPane = () => {
+  const [isLoading, setLoading] = React.useState(false);
   console.log('HeaderPane');
+
+  const saveProcess = () => {
+    setLoading(true);
+    setTimeout(() => {
+      const num = Math.floor(Math.random() * 10);
+      if (num > 5) {
+        message.success('success');
+      } else {
+        message.error('error');
+      }
+      setLoading(false);
+    }, 1000);
+  };
+
   return (
     <div className="logic-ui-header-pane">
       <div className="header-pane-wapper">
@@ -17,11 +26,12 @@ const HeaderPane = () => {
           {/* <span className="header-pane-version">1.1.2</span> */}
         </div>
         <div className="header-pane-right">
-          {/* <div className="">提示</div> */}
-          <div className="">
+          {/* <div className="header-pane-tips">提示</div> */}
+          <div className="header-pane-save">
             <Button type="primary"
               size="middle"
               onClick={saveProcess}
+              loading={isLoading}
             >
               保存
             </Button>
